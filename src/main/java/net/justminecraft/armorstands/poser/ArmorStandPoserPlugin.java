c756b1a4a912652bb412fcf3b6dbb221e50302b3
KK964 <morrkade05@gmail.com>
2021-01-27 13:45:41 -0600

    made base plugin

---
+++ src/main/java/net/justminecraft/armorstands/poser/ArmorStandPoserPlugin.java
@@ -0,0 +1,24 @@
+package net.justminecraft.armorstands.poser;
+
+import net.justminecraft.armorstands.poser.commands.ArmorStand;
+import net.justminecraft.armorstands.poser.commands.SetArmorStandCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class ArmorStandPoserPlugin extends JavaPlugin {
+
+    public static ArmorStandPoserPlugin armorStandPoserPlugin = null;
+
+    @Override
+    public void onEnable() {
+        armorStandPoserPlugin = this;
+        getLogger().info("Just Armor Stands Poser Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Just Armor Stands Poser Disabled!");
+    }
+
+}
