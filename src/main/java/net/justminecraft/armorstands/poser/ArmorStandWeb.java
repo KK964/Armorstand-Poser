ea0578f98d12918713389f608ebfd7c5b5cab8cd
KK964 <morrkade05@gmail.com>
2021-01-30 18:12:42 -0600

    added web

--- src/main/java/net/justminecraft/armorstands/poser/ArmorStandWeb.java
+++ src/main/java/net/justminecraft/armorstands/poser/ArmorStandWeb.java
@@ -2,9 +2,12 @@ package net.justminecraft.armorstands.poser;
 
 import org.bukkit.entity.Entity;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -13,7 +16,7 @@ public class ArmorStandWeb implements Runnable {
     private HashMap<String, Entity> armorStands = new HashMap<>();
 
     public ArmorStandWeb() throws IOException {
-        serverSocket = new ServerSocket(2048);
+        serverSocket = new ServerSocket(2058);
     }
 
     public void run() {
@@ -29,10 +32,11 @@ public class ArmorStandWeb implements Runnable {
 
     public String createHandler(Entity e) {
         String s = "/" + Long.toString(new Random().nextLong(), 16);
+        System.out.println(s);
         if(armorStands.size() > 100)
             armorStands.clear();
         armorStands.put(s, e);
-        return "http://192.99.254.118:" + serverSocket.getLocalPort() + s;
+        return "http://localhost:" + serverSocket.getLocalPort() + s;
     }
 
     class Handler extends Thread {
@@ -45,7 +49,101 @@ public class ArmorStandWeb implements Runnable {
 
         public void run() {
             try {
+                BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                String header;
+                while ((header = reader.readLine()) != null) {
+                    String[] head = header.split(" ");
+                    String s = null;
+                    while ((s = reader.readLine()).length() > 1) ;
+                    URL url = new URL("http://example.com" + head[1]);
+                    String path = url.getPath();
+                    String query = url.getQuery();
+                    String armorStandNbt2 = null;
+                    if(query != null)
+                        for(String q : query.split("&")) {
+                            if(q.startsWith("armorStandNbt="))
+                                armorStandNbt2 = URLDecoder.decode(q.substring("armorStandNbt=".length()), "UTF-8");
+                        }
+                    String armorStandNbt = armorStandNbt2;
+                    if(armorStands.containsKey(path)) {
+                        String content = "An error occurred";
 
+                        if(armorStandNbt == null) {
+                            InputStream in = ArmorStandPoserPlugin.armorStandPoserPlugin.getResource("web/index.htm");
+                            byte[] c = new byte[8192];
+                            int l = in.read(c);
+                            content = new String(c, 0, l, StandardCharsets.UTF_8);
+                        }
+                        out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                        out.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
+                        out.write(("Content-Length: " + content.length() + "\r\n").getBytes());
+                        out.write("Server: PlotMinigames\r\n".getBytes());
+                        out.write("Connection: keep-alive\r\n".getBytes());
+                        out.write("\r\n".getBytes());
+                        out.write(content.getBytes(StandardCharsets.UTF_8));
+                        out.flush();
+                    } else {
+                        System.out.println(path);
+                        String content = "404 Not Found";
+                        if(path == "/style.css" || path == "/js/colorpick.js" || path == "/js/colorpick.css" || path == "/js/main.js") {
+                            String contentType = "text/html";
+                            switch (path) {
+                                case "/style.css": {
+                                    InputStream in = ArmorStandPoserPlugin.armorStandPoserPlugin.getResource("web/style.css");
+                                    byte[] c = new byte[8192];
+                                    int l = in.read(c);
+                                    contentType = "text/css";
+                                    content = new String(c, 0, l, StandardCharsets.UTF_8);
+                                    break;
+                                }
+                                case "/js/colorpick.js": {
+                                    InputStream in = ArmorStandPoserPlugin.armorStandPoserPlugin.getResource("web/js/colorpick.js");
+                                    byte[] c = new byte[8192];
+                                    int l = in.read(c);
+                                    contentType = "text/javascript";
+                                    content = new String(c, 0, l, StandardCharsets.UTF_8);
+                                    break;
+                                }
+                                case "/js/colorpick.css": {
+                                    InputStream in = ArmorStandPoserPlugin.armorStandPoserPlugin.getResource("web/js/colorpick.css");
+                                    byte[] c = new byte[8192];
+                                    int l = in.read(c);
+                                    contentType = "text/css";
+                                    content = new String(c, 0, l, StandardCharsets.UTF_8);
+                                    break;
+                                }
+                                case "/js/main.js": {
+                                    InputStream in = ArmorStandPoserPlugin.armorStandPoserPlugin.getResource("web/js/main.js");
+                                    byte[] c = new byte[8192];
+                                    int l = in.read(c);
+                                    contentType = "text/javascript";
+                                    content = new String(c, 0, l, StandardCharsets.UTF_8);
+                                    break;
+                                }
+                            }
+                            String outWriteType = "Content-Type: " + contentType + "; charset=UTF-8\r\n";
+                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                            out.write(outWriteType.getBytes());
+                            out.write(("Content-Length: " + content.length() + "\r\n").getBytes());
+                            out.write("Server: PlotMinigames\r\n".getBytes());
+                            out.write("Connection: keep-alive\r\n".getBytes());
+                            out.write("\r\n".getBytes());
+                            out.write(content.getBytes(StandardCharsets.UTF_8));
+                            out.flush();
+                        }
+                        else {
+                            out.write("HTTP/1.1 404 NOT FOUND\r\n".getBytes());
+                            out.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
+                            out.write(("Content-Length: " + content.length() + "\r\n").getBytes());
+                            out.write("Server: PlotMinigames\r\n".getBytes());
+                            out.write("Connection: keep-alive\r\n".getBytes());
+                            out.write("\r\n".getBytes());
+                            out.write(content.getBytes(StandardCharsets.UTF_8));
+                            out.flush();
+                        }
+                    }
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
