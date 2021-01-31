db22b21d16a2bae1746e6eb2efa2c883ab8a6222
KK964 <morrkade05@gmail.com>
2021-01-31 16:22:14 -0600

    made way to get entity nbt

--- src/main/java/net/justminecraft/armorstands/poser/NBTHandler.java
+++ src/main/java/net/justminecraft/armorstands/poser/NBTHandler.java
@@ -1,22 +1,33 @@
 package net.justminecraft.armorstands.poser;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import org.bukkit.entity.ArmorStand;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.v1_16_R3.MojangsonParser;
+import net.minecraft.server.v1_16_R3.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
 import org.bukkit.entity.Entity;
 
 public class NBTHandler {
-    public JsonObject getNBT(ArmorStand armorStand) {
-        JsonObject armorStandNBT = new JsonObject();
+    public String getNBT(Entity e) {
+        net.minecraft.server.v1_16_R3.Entity nms = ((CraftEntity) e).getHandle();
+        NBTTagCompound nbt = new NBTTagCompound();
+        nms.a_(nbt);
+        System.out.println(nbt);
+        return nbt.toString();
+    }
 
-        ArmorStandData as = new ArmorStandData(armorStand);
-        if(as.showArms) armorStandNBT.addProperty("ShowArms", true);
-        if(!as.gravity) armorStandNBT.addProperty("NoGravity", true);
-        if(!as.basePlate) armorStandNBT.addProperty("NoBasePlate", true);
-        if(as.small) armorStandNBT.addProperty("Small", true);
-        if(!as.visible) armorStandNBT.addProperty("Invisible", true);
-        if(as.invulnerable) armorStandNBT.addProperty("Invulnerable", true);
+    public void setNBT(Entity e, String value) {
+        net.minecraft.server.v1_16_R3.Entity nms = ((CraftEntity) e).getHandle();
+        try {
+            NBTTagCompound nbtv = MojangsonParser.parse(value);
+            nms.load(nbtv);
+        } catch (CommandSyntaxException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-        return armorStandNBT;
+    public String getWebFormattedNBT(Entity e) {
+        JsonObject jsonObject = new JsonObject();
+        return jsonObject.toString();
     }
 }
