631e6a76a30a12aec51e039dcf2a108f3c6e7fee
KK964 <morrkade05@gmail.com>
2021-02-05 19:31:43 -0600

    Update NBTHandler.java

--- src/main/java/net/justminecraft/armorstands/poser/NBTHandler.java
+++ src/main/java/net/justminecraft/armorstands/poser/NBTHandler.java
@@ -7,7 +7,9 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.v1_16_R3.MojangsonParser;
 import net.minecraft.server.v1_16_R3.NBTTagCompound;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,7 +57,7 @@ public class NBTHandler {
         return nbtOld.toString();
     }
 
-    public String getNBTWebInput(Entity e) {
+    public JsonObject getNBTWebInput(Entity e) {
         ArrayList<String> validNBT = new ArrayList<>(Arrays.asList("Invisible",
                 "Invulnerable",
                 "PersistenceRequired",
@@ -80,18 +82,49 @@ public class NBTHandler {
         JsonObject formattedNBT = new JsonObject();
         for(String s : keys) {
             if(validNBT.contains(s)) {
-                String regBrackets = "(\\[|\\])";
                 switch (s) {
+                    case "HandItems": {
+                        //"HandItems":"[{id:\"minecraft:pink_stained_glass\",Count:1b,tag:{display:{Name:'{\"text\":\"Support Pinky\"}'}}},{}]"
+                        String handItems = nbt.get(s).asString();
+                        handItems = handItems.substring(1);
+                        handItems = handItems.substring(0, handItems.length() -1);
+                        String[] items = handItems.split("(?<=(\\})),");
+                        for(String i : items) {
+                            i = i.substring(1);
+                            i = i.substring(0, i.length() -1);
+                            String[] iA = i.split("");
+                        }
+                    }
+                    case "Pose": {
+                        //"Pose":"{RightArm:[0.0f,0.0f,0.0f],LeftLeg:[0.0f,0.0f,0.0f],LeftArm:[0.0f,0.0f,0.0f],RightLeg:[0.0f,0.0f,0.0f]}"
+                        String pose = nbt.get(s).asString();
+                        pose = pose.substring(1);
+                        pose = pose.substring(0, pose.length() -1);
+                        String[] poseArray = pose.split("(?<=(\\])),");
+                        JsonObject poseObject = new JsonObject();
+                        for(String p : poseArray) {
+                            String[] pO = p.split(":");
+                            pO[1] = pO[1].replaceAll("(\\[|\\])", "");
+                            String[] pOI = pO[1].split(",");
+                            JsonArray pOIArray = new JsonArray();
+                            for(String pI : pOI) {
+                                pOIArray.add(pI);
+                            }
+                            poseObject.add(pO[0], pOIArray);
+                        }
+                        formattedNBT.add(s, poseObject);
+                        break;
+                    }
                     case "Rotation": {
                         //"Rotation":"[73.0f,0.0f]"
-                        String customRotation = nbt.get(s).toString();
-                        customRotation = customRotation.replace(regBrackets, "");
+                        String customRotation = nbt.get(s).asString();
+                        customRotation = customRotation.replaceAll("(\\[|\\])", "");
                         String[] rotationAgs = customRotation.split(",");
                         JsonArray rotationArray = new JsonArray();
                         for(String rot : rotationAgs) {
                             rotationArray.add(rot);
                         }
-                        formattedNBT.add("Rotation", rotationArray);
+                        formattedNBT.add(s, rotationArray);
                         break;
                     }
                     case "CustomName": {
@@ -99,15 +132,15 @@ public class NBTHandler {
                         String customName = nbt.get(s).asString();
                         customName = customName.substring(1);
                         customName = customName.substring(0, customName.length() - 1);
+                        customName = customName.replaceAll("\"", "");
                         String[] nameArgs = customName.split(",");
-                        JsonArray nameArray = new JsonArray();
+                        JsonObject nameObj = new JsonObject();
                         for(String n : nameArgs) {
-                            String[] nArgs = n.split(":");
-                            JsonObject nO = new JsonObject();
-                            nO.addProperty(nArgs[0], nArgs[1]);
-                            nameArray.add(nO);
+                            System.out.println(n);
+                            String[] nm = n.split(":");
+                            nameObj.addProperty(nm[0], nm[1]);
                         }
-                        formattedNBT.add("name", nameArray);
+                        formattedNBT.add(s, nameObj);
                         break;
                     }
                     default:
@@ -117,6 +150,24 @@ public class NBTHandler {
             }
         }
 
-        return formattedNBT.toString();
+        return formattedNBT;
+    }
+
+    public String itemStackToNBT(ItemStack itemStack) {
+        net.minecraft.server.v1_16_R3.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+        Set<String> keys = new HashSet<>(nmsStack.getTag().getKeys());
+
+        return null;
+    }
+
+    public static String replaceLast(String string, String toReplace, String replacement) {
+        int pos = string.lastIndexOf(toReplace);
+        if (pos > -1) {
+            return string.substring(0, pos)
+                    + replacement
+                    + string.substring(pos + toReplace.length());
+        } else {
+            return string;
+        }
     }
 }
